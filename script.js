bcda5175a2cf960323cd994e951165fe90a28a4a
WellPlacedCactus <abowlasoup@gmail.com>
2023-02-24 19:45:38 -0600

    Implemented 2D lighting

--- script.js
+++ script.js
@@ -5,36 +5,39 @@
   const c = canvas.getContext('2d');
   const mouse = {};
   const parts = [];
+  const images = [];
   const pLoader = new PLoader();
-  const image = pLoader.loadRect(240);
+  for (let i = 0; i < 360; i += 60) {
+    images.push(pLoader.loadImage(16, i));
+  }
 
   const loop = () => {
 
-    for (let i = 0; i < 1; i++) {
+    for (let i = 0; i < images.length; i++) {
       parts.push({
-        x: canvas.width / 2,
+        x: canvas.width / images.length * (i + 1) - canvas.width / images.length / 2,
         y: canvas.height / 2,
         vx: Math.random() - 0.5,
         vy: Math.random() - 1,
-        s: 1
+        s: 1,
+        image: images[i]
       });
     }
 
-    c.globalCompositeOperation = 'lighter';
-    
     c.clearRect(0, 0, canvas.width, canvas.height);
 
     for (let i = parts.length - 1; i >= 0; --i) {
       const p = parts[i];
-      const { x, y, vx, vy, s } = p;
+      const { x, y, vx, vy, s, image } = p;
+      p.vy += 0.01;
       p.x += vx;
       p.y += vy;
-      p.s -= 0.01;
+      p.s -= Math.random() * 0.01;
       c.drawImage(image,
                   x - image.width * s / 2,
                   y - image.height * s / 2,
                   image.width * s,
-                  image.height  * s);
+                  image.height * s);
       if (s < 0) parts.splice(i, 1);
     }
 
@@ -44,6 +47,7 @@
   addEventListener('load', () => {
     canvas.width = innerWidth;
     canvas.height = innerHeight;
+    c.globalCompositeOperation = 'lighter';
     mouse.x = 0;
     mouse.y = 0;
     mouse.down = false;
@@ -53,6 +57,7 @@
   addEventListener('resize', () => {
     canvas.width = innerWidth;
     canvas.height = innerHeight;
+    c.globalCompositeOperation = 'lighter';
   });
 
   addEventListener('mousemove', ({x, y}) => {
--- scripts/pLoader.js
+++ scripts/pLoader.js
@@ -3,17 +3,18 @@ class PLoader {
 
   constructor() {}
 
-  loadRect(h) {
+  loadImage(s, h) {
     const canvas = document.createElement('canvas');
     const c = canvas.getContext('2d');
-    canvas.width = 100;
-    canvas.height = 100;
+    const canvasSize = 1000;
+    canvas.width = canvasSize;
+    canvas.height = canvasSize;
     c.fillStyle = `hsl(${h}, 50%, 50%)`;
     c.shadowColor = `hsl(${h + 5}, 50%, 50%)`;
     c.shadowBlur = 25;
-    c.fillRect(canvas.width / 2 - 8,
-               canvas.height / 2 - 8,
-               16, 16);
+    c.fillRect((canvas.width - s) / 2,
+               (canvas.height - s) / 2,
+               s, s);
     return canvas;
   }
 }
